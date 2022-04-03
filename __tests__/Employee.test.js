d33d953c7f028e0e1b8e21552dbf4c98913c1053
Moonman9 <zzepold97@gmail.com>
2022-04-02 17:11:11 -0700

    employee object created and passed test

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -1,8 +1,34 @@
 const Employee = require('../lib/Employee')
+
 test('creates an employee object', () => {
     const employee = new Employee('Stitch', 626, 'stitch626@hotmail.com');
 
     expect(employee.name).toBe('Stitch');
     expect(employee.id).toEqual(626);
-    expect(employee.email).toEqual('stitch626@hotmail.com');
+    expect(employee.email).toBe('stitch626@hotmail.com');
 });
+
+
+test('get employee name', () => {
+    const employee = new Employee('Stitch', 626, 'stitch626@hotmail.com');
+
+    expect(employee.getName()).toBe('Stitch');
+});
+
+test('get employee id', () => {
+    const employee = new Employee('Stitch', 626, 'stitch626@hotmail.com');
+
+    expect(employee.getId()).toEqual(626);
+});
+
+test('get employee email', () => {
+    const employee = new Employee('Stitch', 626, 'stitch626@hotmail.com');
+
+    expect(employee.getEmail()).toBe('stitch626@hotmail.com');
+});
+
+test('get role for the employee', () => {
+    const employee = new Employee('Stitch', 626, 'stitch626@hotmail.com');
+
+    expect(employee.getRole()).toEqual(expect.any(String));
+})
