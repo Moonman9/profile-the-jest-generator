06b58c43efb5e4cd9015ee956c7fdb63c4f5f109
Moonman9 <zzepold97@gmail.com>
2022-04-04 18:46:45 -0700

    asks series of questions based on role selection and validates responses then pushes them to array

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const Manager = require('./lib/Manager');
 const Engineer = require('./lib/Engineer');
 const Intern = require('./lib/Intern');
 
+
 const teamArray = [];
 
 function addEmployee () {
@@ -14,15 +15,18 @@ function addEmployee () {
         message: 'Which type of employee would you like to add to the team?',
         choices: ['Manager', 'Engineer', 'Intern', 'Finalize team generator']
     }]).then(function(choice) {
-        switch(choice.addSelectEmployee) {
-            case 'Manager': addManager();
+        switch(choice.employeeChoices) {
+            case 'Manager': 
+                addManager();
                 break;
-            case 'Engineer': addEngineer();
+            case 'Engineer': 
+                addEngineer();
                 break;
-            case 'Intern': addIntern();
+            case 'Intern': 
+                addIntern();
                 break;
 
-            default: createHtml();
+            // default: createHtml();
         }
     })
 }
@@ -89,3 +93,140 @@ function addManager() {
     });
 }
 
+function addEngineer() {
+    inquirer.prompt([
+        {
+            type: 'input',
+            message: 'What is the name of the engineer?',
+            name: 'engineerName',
+            validate: nameInput => {
+                if (nameInput) {
+                    return true;
+                } else {
+                    console.log("Please enter the engineer's name");
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'input',
+            message: "What is the engineer's ID number?",
+            name: 'engineerId',
+            validate: nameInput => {
+                if (isNaN(nameInput)) {
+                    console.log("Please enter a numerical value for the engineer's ID");
+                    return false;
+                } else {
+                    return true;
+                }
+            }
+        },
+        {
+            type: 'input',
+            message: "What is the engineer's email address?",
+            name: 'engineerEmail',
+            validate: email => {
+                valid = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email)
+                if (valid) {
+                    return true
+                } else {
+                    console.log("Please enter the appropriate email address for the engineer");
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'input',
+            message: "What is the engineer's GitHub username?",
+            name: 'engineerGitHub',
+            validate: nameInput => {
+                if (nameInput) {
+                    return true;
+                } else {
+                    console.log("Please enter the manager's name");
+                    return false;
+                }
+            }
+        }
+    ]).then(answers => {
+        const engineer = new Engineer(answers.engineerName, answers.engineerId, answers.engineerEmail, answers.engineerGitHub);
+        teamArray.push(engineer);
+        addEmployee();
+    })
+}
+
+function addIntern() {
+    inquirer.prompt([
+        {
+            type: 'input',
+            message: 'What is the name of the intern?',
+            name: 'internName',
+            validate: nameInput => {
+                if (nameInput) {
+                    return true;
+                } else {
+                    console.log("Please enter the intern's name");
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'input',
+            message: "What is the intern's ID number?",
+            name: 'internId',
+            validate: nameInput => {
+                if (isNaN(nameInput)) {
+                    console.log("Please enter a numerical value for the intern's ID");
+                    return false;
+                } else {
+                    return true;
+                }
+            }
+        },
+        {
+            type: 'input',
+            message: "What is the intern's email address?",
+            name: 'internEmail',
+            validate: email => {
+                valid = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email)
+                if (valid) {
+                    return true
+                } else {
+                    console.log("Please enter the appropriate email address for the intern");
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'input',
+            message: "What is the intern's GitHub username?",
+            name: 'internGitHub',
+            validate: nameInput => {
+                if (nameInput) {
+                    return true;
+                } else {
+                    console.log("Please enter the intern's name");
+                    return false;
+                }
+            }
+        }
+    ]).then(answers => {
+        const intern = new Intern(answers.internName, answers.internId, answers.internEmail, answers.internSchool);
+        teamArray.push(intern);
+        addEmployee();
+    })
+}
+
+// function createHtml() {
+//     fs.writeFile('./dist/index.html', data, err => {
+//         if (err) {
+//             console.log(err);
+//             return
+//         } else {
+//             console.log("Team profile page has been generated.")
+//         }
+//     })
+// }
+
+
+addEmployee();
